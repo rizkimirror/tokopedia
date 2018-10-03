98f373dc0b109cd43fcec993883b604d4fcbf491
Rizki Kurniawan <rizkimirror@MacBook-Pro-Rizki.local>
2018-10-03 21:01:35 +0700

    first commit

---
+++ src/Pantek.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Pantek extends Component {
+  render() {
+    return (
+      <div>
+        <p>Aplikasi Pantek</p>
+      </div>
+    )
+  }
+}
+
+
